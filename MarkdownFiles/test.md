b934025b2554537bdf46ac1fcae8c0d4c74910e7
Ford Stone <69664915+ZookaDook@users.noreply.github.com>
2026-05-25 19:22:55 -0700

    zoom images on click

--- MarkdownFiles/test.md
+++ MarkdownFiles/test.md
@@ -2,7 +2,12 @@
 
 here is some text
 
-<img src="testImage.jpg" class = img alt="test">
+
+<img src="\MarkdownFiles\testImage.jpg" class="img imgCanFocus" alt="test">
+<img src="\MarkdownFiles\testImage.jpg" class="img imgCanFocus" alt="test">
+<img src="\MarkdownFiles\testImage.jpg" class="img imgCanFocus" alt="test">
+<img src="\MarkdownFiles\testImage.jpg" class="img imgCanFocus" alt="test">
+<img src="\MarkdownFiles\testImage.jpg" class="img imgCanFocus" alt="test">
 
 dog dogsa 
 asdasd
--- globals.js
+++ globals.js
@@ -1,6 +1,10 @@
 /* globals for the project */
 
 
+const imgFocusClass = ".imgCanFocus";
+
+let placeholder = null;
+
 // get a param from the url
 function getParam(Qname){
     try{
@@ -10,4 +14,67 @@ function getParam(Qname){
     catch{
         return "none";
     }
+}
+
+// adds the class to allow focusing to all the children of an element
+// mostly used for adding zoom to images in .md files
+function makeChildImgsFocusable(element){
+    element.querySelectorAll('img').forEach(img => {
+        img.addEventListener('click', toggleFocus);
+    });
+}
+
+// add the zoom to all other images with the calss
+// this hits the stuff that loaded in to begin with
+$(imgFocusClass).on("click",  toggleFocus);
+
+// fucntion to zoom in and out
+function toggleFocus(){
+    // get the element and toggle the class
+    let element = $(this);
+    focusImage(element);
+}
+
+function focusImage(img){
+    const zoomImg = $(img.clone());
+    const offset = img.offset();
+
+    placeholder = zoomImg;
+    zoomImg.on("click", unFocusImage);
+
+    zoomImg.css({
+        'cursor': 'zoom-out',
+        'position': 'absolute',
+        'top': offset.top + 'px',
+        'left': offset.left + 'px',
+        'width': img.outerWidth(),    
+        'height': img.outerHeight(),  
+        'margin': '0',                
+    });
+
+    $('body').append(zoomImg);
+
+    zoomImg[0].offsetWidth;
+
+    zoomImg.css({
+        'transform': 'scale(2)'
+    });
+    zoomImg.one('transitionend', () => { 
+        zoomImg.css({
+            'top': '50%',
+            'left': '50%',
+            'transform': 'translate(-50%, -50%) scale(2)'
+        });
+    });
+    
+    img.addClass('imgHidden');
+}
+
+function unFocusImage(){
+    placeholder.css({
+        'top': '50%',
+        'left': '50%',
+        'opacity': '0.5',
+        'transform': 'scale(1)'               
+    });
 }
